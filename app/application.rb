5fa3d0b488e70ee1e633dd3463517eada6e9d557
John Zonneveld <jjczon@gmail.com>
2020-05-17 11:58:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-     item = 
+     item = req.param
     else
       resp.write "Path Not Found"
     end
